140d6059d0718176dbf29b32d814dced2e82c46e
duruduhnunes <duruduh883@gmail.com>
2025-10-11 18:26:27 -0300

    ajeitei a navBar e ajeitei uns detalhes no inicio

--- src/app/inicio/page.tsx
+++ src/app/inicio/page.tsx
@@ -1,12 +1,59 @@
+'use client'
+
+import { motion } from "framer-motion";
 export default function Inicio() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen ">
-     <div className="">
-       
-     </div>
-     <div className="">
+    <motion.div className="flex min-h-screen -mt-36" 
+    initial={{ opacity: 0, y: 100}}
+    animate={{opacity:1, y: 0}}
+     transition={{
+    ease: "easeOut",
+    duration: 0.8                   
+  }}
+    >
+      
+      <div className="w-1/2 flex flex-col items-center justify-center">
+        <img
+          src="/parmegianaa.png"
+          alt="Prato"
+          
+          className="max-w-full h-auto animate-float"
+        />
+     
+     
+        <div className="flex  items-start gap-3.5">
+  <div>
+    ARROZ
+  </div>
+  <div className="relative before:content-['•'] before:absolute before:-left-4 ml-3.5">
+    FRANGO OU CARNE
+  </div>
+  <div className="relative before:content-['•'] before:absolute before:-left-4 ml-3.5">
+    SALADA
+  </div>
+</div>
 
-     </div>
-    </div>
-  )
+       
+      </div>
+        
+
+     
+      <div className="w-1/2 flex flex-col items-start justify-center p-8 gap-5">
+        <p className=" text-gray-400 text-2xl font-mono mb-4">#1 Dessa Semana</p>
+        <p className=" text-white uppercase font-mono text-4xl">
+          Parmegiana 
+        </p>
+        <p className=" text-white uppercase font-mono text-4xl">
+          é nordeste com um tiquinho da Itália… 
+        </p>
+        <p className="text-4xl text-white font-mono uppercase">
+          mas com gosto de casa de vó!
+        </p>
+        <div className="inline text-amber-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat iste rem nihil, blanditiis fuga incidunt maiores error atque ullam, dolor qui excepturi temporibus optio similique quos autem ut repellat nulla.</div>
+                    <button className="w-36 h-13 rounded-lg text-white cursor-pointer font-extralight mt-5 shadow-2xl bg-amber-600 px-2.5 py-2.5 flex items-center justify-center hover:bg-amber-500">ver o cardapio</button>
+
+      </div>
+
+    </motion.div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,15 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [isActive, setIsActive] = useState("Início") 
   return (
     <nav className="bg-gray-900 text-white px-6 py-4 shadow-md">
       <div className="flex items-center justify-between max-w-6xl mx-auto">
-        {/* Logo */}
         <div className="flex items-center">
-          <Image 
+          <Image
             src="/Rectangle.png" 
             alt="Logo" 
             width={150} 
@@ -16,33 +19,60 @@ export default function Navbar() {
           <div className="text-white text-2xl font-bold">MARMININO</div>
         </div>
         
-        {/* Links de navegação */}
         <div className="flex items-center gap-6">
-          <a 
-            href="#" 
-            className="text-white hover:text-gray-300 transition-colors duration-200 font-medium"
-          >
-            Home
-          </a>
-          <a 
-            href="#" 
-            className="text-white hover:text-gray-300 transition-colors duration-200 font-medium"
-          >
-            About
-          </a>
-          <a 
-            href="#" 
-            className="text-white hover:text-gray-300 transition-colors duration-200 font-medium"
-          >
-            Menu
-          </a>
-          <a 
-            href="#" 
-            className="text-white hover:text-gray-300 transition-colors duration-200 font-medium"
-          >
-            New
-          </a>
-        </div>
+ <a
+  href="#"
+  onClick={() => setIsActive("Início")}
+  className={`relative text-white cursor-pointer text-lg font-medium
+    after:content-[''] after:absolute after:w-0 after:h-[2px] after:left-0 after:-bottom-1 after:bg-amber-600 after:transition-all after:duration-300 hover:after:w-full
+    ${isActive === "Início"
+      ? "after:w-full"
+      : "after:w-0 hover:after:w-full"
+    }`
+  }
+>
+  Início
+</a>
+
+
+  <a
+  href="#"
+  onClick={() => setIsActive("Sobre")}
+  className={`relative text-white cursor-pointer text-lg font-medium
+    after:content-[''] after:absolute after:h-[2px] after:left-0 after:-bottom-1 after:bg-amber-600 after:transition-all after:duration-300
+    ${isActive === "Sobre" ? "after:w-full" : "after:w-0 hover:after:w-full"}
+  `}
+>
+  Sobre
+</a>
+
+
+  <a
+    href="#"
+    onClick={() => setIsActive("Cardápio")}
+    className={`relative text-white cursor-pointer text-lg font_medium
+      after:content-[''] after:absolute after:h-[2px] after:left-0 after:-bottom-1 after:bg-amber-600 after:transition-all after:duration-300
+      ${isActive === "Cardápio" ? "after:w-full" : "after:w-0 hover:after:w-full"}
+      `}
+  >
+    Cardápio
+  </a>
+
+  <a
+    href="#"
+    onClick={() => setIsActive("Novidades")}
+    className={`relative text-white cursor-pointer text-lg font-medium 
+               after:content-[''] after:absolute after:h-[2px] after:left-0 after:-bottom-1 
+               after:bg-amber-600 after:transition-all after:duration-400
+               ${isActive === "Novidades"
+                ? "after:w-full"
+                : "after:w-0 hover:after:w-full"
+               }`}
+  >
+    Novidades
+  </a>
+</div>
+
       </div>
     </nav>
   );
