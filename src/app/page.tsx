4d2f7a9b3f511b9d32cd3ff1a2c5bac9f02d6ffe
duruduhnunes <duruduh883@gmail.com>
2025-11-09 11:05:03 -0300

    refactor: Clean up layout in Home and Inicio components for improved readability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-import Image from "next/image";
 import Inicio from "./inicio/page";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900"><Inicio /></div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+      <Inicio />
+    </div>
   );
 }
